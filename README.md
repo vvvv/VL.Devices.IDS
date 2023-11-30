365316d393001699cb398ca3303112dee4c1e430
sebescudie <accounts@sebescudie.fr>
2023-11-30 12:11:22 +0100

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# VL.IDSPeak
+
+## Stuff I don't want to spend another ten days looking for
+
+- `C:\Program Files\IDS\ids_peak\generic_sdk\samples\source\csharp`
+- `C:\Program Files\IDS\ids_peak\generic_sdk\samples\bin\x86_64\ids_peak_dotnet.dll`
---
+++ csharp/VL.IDSPeak.Utils/Whatever.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL.IDSPeak.Utils
+{
+    [ProcessNode]
+    public class Whatever
+    {
+        public void Blah()
+        {
+            //s
+        }
+    }
+}
