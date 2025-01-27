cc02aa68b8a9fe142c68828c0ae87c8816a185d3
sebl <sebl@schnellebuntebilder.de>
2025-01-27 14:33:40 +0100

    defaultUserSet renamed to "UserSet Default" and is now an enum. No more need for Nullable.

--- src/VideoIn.cs
+++ src/VideoIn.cs
@@ -6,6 +6,7 @@ using VL.Model;
 using System.Reactive.Subjects;
 using System.Reactive.Linq;
 using VL.Devices.IDS.Advanced;
+using static VL.Devices.IDS.Enumerations;
 
 namespace VL.Devices.IDS
 {
@@ -22,7 +23,7 @@ namespace VL.Devices.IDS
         private int _fps;
         private IConfiguration? _configuration;
         private bool _enabled;
-        private string? _defaultUserSet;
+        private UserSetDefault _defaultUserSet;
 
         internal string Info { get; set; } = "";
         internal Spread<PropertyInfo> PropertyInfos { get; set; } = new SpreadBuilder<PropertyInfo>().ToSpread();
@@ -39,19 +40,19 @@ namespace VL.Devices.IDS
             [DefaultValue("640, 480")] Int2 resolution,
             [DefaultValue("30")] int FPS,
             IConfiguration configuration,
+            [Pin(Name = "UserSet Default", Visibility = PinVisibility.Optional), DefaultValue("Default")] UserSetDefault UserSetDefault,
             [DefaultValue("true")] bool enabled,
-            [Pin(Visibility = PinVisibility.Optional), DefaultValue("Default")] string defaultUserSet,
             out string Info)
         {
             // By comparing the descriptor we can be sure that on re-connect of the device we see the change
-            if (device?.Tag != _device || resolution != _resolution || FPS != _fps || configuration != _configuration || enabled != _enabled || defaultUserSet != _defaultUserSet)
+            if (device?.Tag != _device || resolution != _resolution || FPS != _fps || configuration != _configuration || enabled != _enabled || UserSetDefault != _defaultUserSet)
             {
                 _device = device?.Tag as DeviceDescriptor;
                 _resolution = resolution;
                 _fps = FPS;
                 _configuration = configuration;
                 _enabled = enabled;
-                _defaultUserSet = defaultUserSet;
+                _defaultUserSet = UserSetDefault;
                 _changedTicket++;
             }
 
@@ -72,7 +73,7 @@ namespace VL.Devices.IDS
 
             try
             {
-                var result = Acquisition.Start(this, device, _logger, _resolution, _fps, _configuration, _defaultUserSet ?? "Default");
+                var result = Acquisition.Start(this, device, _logger, _resolution, _fps, _configuration, _defaultUserSet.ToString() ?? "Default");
                 _aquicitionStarted.OnNext(result);
                 return result;
             }
